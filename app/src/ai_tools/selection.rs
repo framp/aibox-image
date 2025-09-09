2f11d9597a101dbc2653f37d0e832c4ef5db3c12
Nicola Bovolato <nicola.bovolato@gmail.com>
2025-09-09 12:25:46 +0300

    perf: spawn network requests off ui thread

--- app/src/ai_tools/selection.rs
+++ app/src/ai_tools/selection.rs
@@ -2,26 +2,31 @@ use std::sync::mpsc::{self, Receiver, Sender};
 
 use eframe::egui::{Button, TextEdit, Ui};
 use image::DynamicImage;
-use serde_bytes::ByteBuf;
 
-use crate::{ai_tools::selection, image_canvas::SharedCanvas};
+use crate::image_canvas::SharedCanvas;
 
 use super::zmq;
 
 pub struct SelectionTool {
     input: String,
     canvas: SharedCanvas,
+    tx: Sender<Vec<DynamicImage>>,
+    rx: Receiver<Vec<DynamicImage>>,
 }
 
 impl SelectionTool {
     pub fn new(canvas: SharedCanvas) -> Self {
+        let (tx, rx) = mpsc::channel();
+
         Self {
             input: String::new(),
             canvas,
+            tx,
+            rx,
         }
     }
 
-    fn fetch(&self, ctx: &eframe::egui::Context) {
+    fn fetch(&self) {
         let image_path = {
             let canvas_ref = self.canvas.borrow();
             canvas_ref
@@ -31,26 +36,30 @@ impl SelectionTool {
                 .to_string_lossy()
                 .to_string()
         };
+        let input = self.input.clone();
+        let tx = self.tx.clone();
 
-        let response = zmq::request_response(
-            "tcp://127.0.0.1:5558",
-            zmq::Request::ImageSelection {
-                prompt: self.input.to_owned(),
-                image_path: image_path.to_owned(),
-            },
-        )
-        .unwrap();
+        std::thread::spawn(move || {
+            let response = zmq::request_response(
+                "tcp://127.0.0.1:5558",
+                zmq::Request::ImageSelection {
+                    prompt: input,
+                    image_path: image_path,
+                },
+            )
+            .unwrap();
 
-        if let zmq::Response::Success(payload) = response {
-            if let zmq::ImageSelectionPayload { masks } = payload {
-                let selections: Vec<DynamicImage> = masks
-                    .into_iter()
-                    .filter_map(|mask_bytes| image::load_from_memory(&mask_bytes).ok())
-                    .collect();
+            if let zmq::Response::Success(payload) = response {
+                if let zmq::ImageSelectionPayload { masks } = payload {
+                    let selections: Vec<DynamicImage> = masks
+                        .into_iter()
+                        .filter_map(|mask_bytes| image::load_from_memory(&mask_bytes).ok())
+                        .collect();
 
-                self.canvas.borrow_mut().set_selections(selections, ctx);
+                    let _ = tx.send(selections);
+                }
             }
-        }
+        });
     }
 }
 
@@ -62,7 +71,7 @@ impl super::Tool for SelectionTool {
 
         let submit = ui.add(Button::new("Submit"));
         if submit.clicked() {
-            self.fetch(ui.ctx());
+            self.fetch();
         }
 
         let clear = ui.add(Button::new("Clear"));
@@ -70,6 +79,12 @@ impl super::Tool for SelectionTool {
             self.canvas
                 .borrow_mut()
                 .set_selections(Vec::new(), ui.ctx());
+        }
+
+        if let Ok(selections) = self.rx.try_recv() {
+            self.canvas
+                .borrow_mut()
+                .set_selections(selections, ui.ctx());
         }
     }
 }
